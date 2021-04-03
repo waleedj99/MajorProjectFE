32de63806fee8a599f930d6eb1aba78596f0c589
Waleed Jameel Hyderi <waleedj1699@gmail.com>
2021-04-03 17:09:33 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import "./styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Col, Row } from "react-bootstrap";
+import { Col, Row, Container } from "react-bootstrap";
 import { useState } from "react";
 import { UserContext } from "./Context/UserContext";
 import StudentPage from "./Components/StudentPage";
 import TeacherPage from "./Components/TeacherPage";
 import NavbarComponent from "./Components/NavbarComponent";
 import LoginForm from "./Components/LoginForm";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
 export default function App() {
-  const [isTypeStudent, setStudentType] = useState(false);
+  const [isTypeStudent, setStudentType] = useState(true);
   const [loginToken, setLoginToken] = useState(
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IjFkczE3Y3M4MDAiLCJ1c2VyVHlwZSI6ImF0dGVuZGVlIiwiaWF0IjoxNjE3MTgyMjcxfQ.jPyyqwmPs9UadO7k7fhquYZQrgwlveGnDDnCJZv7kqE"
   );
@@ -18,19 +27,26 @@ export default function App() {
         value={{
           isTypeStudent: isTypeStudent,
           background_color: "#9FC0FF",
-          card_color: "#126072",
-          loginToken: loginToken
+          card_color: "#126072"
         }}
       >
-        <Col md={5}>
-          <LoginForm setLoginToken={setLoginToken} />
-        </Col>
-        <NavbarComponent />
-        {isTypeStudent ? (
-          <StudentPage loginToken={loginToken} />
-        ) : (
-          <TeacherPage loginToken={loginToken} />
-        )}
+        <Router>
+          <Switch>
+            <Route path="/login">
+              <Container>
+                <Row>
+                  <Col md={5}>
+                    <LoginForm setLoginToken={setLoginToken} />
+                  </Col>
+                </Row>
+              </Container>
+            </Route>
+            <Route path="/">
+              <NavbarComponent />
+              {isTypeStudent ? <StudentPage /> : <TeacherPage />}
+            </Route>
+          </Switch>
+        </Router>
       </UserContext.Provider>
     </div>
   );
